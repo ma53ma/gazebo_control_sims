2cafd56ac3c3648c34d223c68f4a5308470c1f36
Sridhar701Pitt <sridhar.pitt@gmail.com>
2021-10-12 03:28:32 -0400

    Fixed the x,y referencing to instead use the transformed x_transformed and y_transformed values (still some confusion/fixes might have to be solved for better understanding). Added maximum velocity constraints and normalized and scalerd the vw vector to the limit (only when exceeded). Instead of scaling the attractor field with distance, now it is uniform all over the map (except the goal location when it will stop instead). Robot now storps when it is within 0.25 meters (hardcoded and thus must be changed to a constant in the future for better referencing) to the goal location

--- scripts/APF_controller.py
+++ scripts/APF_controller.py
@@ -7,6 +7,8 @@ from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point, Twist
 
 
+# To reset position, Use: $rosservice call /gazebo/reset_simulation "{}"
+
 x = 0
 y = 0
 theta = 0
@@ -24,9 +26,13 @@ def odom_callback(odom_msg):
     rot_q = odom_msg.pose.pose.orientation
 
     (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-    #rospy.loginfo('x is ${0}'.format(x))
-    #rospy.loginfo('y is ${0}'.format(y))
-    #rospy.loginfo('theta is ${0}'.format(theta))
+    # rospy.loginfo('x is ${0}'.format(x))
+    # rospy.loginfo('y is ${0}'.format(y))
+    # rospy.loginfo('theta is ${0}'.format(theta))
+    # distance between the "point infront of the robot" (used to calculate v,w) and the robot's center
+    # Potential CONFUSION/FIX: Verify weather this length and the formulated equation/matrices actually corresponds to the distance between the center axle (between the two wheels) and point 
+    #                      or between the robot's center and the point and if the matrices are correctly adjusted to match this equation
+    l = 0.15
     goal = [2, 1]
     obsts = [[0, 0],
              [1.15, 0],
@@ -38,35 +44,41 @@ def odom_callback(odom_msg):
              [0, -1.15],
              [-1.15, -1.15]]
 
+    # Using x_transformed and y_transformed as the actual coordinates of the robot instead of x,y to accomodate transformation to v,w
+    x_transformed = x + l*np.cos(theta)
+    y_transformed = y + l*np.sin(theta)
 
-    K_att = 0.5
+    K_att = 3
     K_rep = 2
-    vel_x_att = K_att*(x - goal[0])
-    vel_y_att = K_att*(y - goal[1])
-    #rospy.loginfo('vel_x_att is ${0}'.format(vel_x_att))
-    #rospy.loginfo('vel_y_att is ${0}'.format(vel_y_att))
+
+    goal_direction = np.subtract([x_transformed,y_transformed], goal)
+    # Unit vector in the direction of the goal
+    goal_direction_hat = goal_direction/np.linalg.norm(goal_direction)
+    # Vel_att will have an uniform magnitude on all points of the map 
+    vel_att = np.reshape(K_att*goal_direction_hat, (2,1))
+
     D_obs = 0.15
     rho_0 = 0.2
-    vel_att = np.array([[vel_x_att],[vel_y_att]])
     vel_rep = np.array([0.0, 0.0])
-    #rospy.loginfo('vel_att is ${0}'.format(vel_att))
+    rospy.loginfo('vel_att is ${0}'.format(vel_att))
     obst_cnt = 0
     for i in range(0, len(obsts)):
         obst = np.array(obsts[i])
-        state = np.array([x,y])
+        state = np.array([x_transformed,y_transformed])
         rho_x = np.linalg.norm(state - obst) - D_obs
         #rospy.loginfo('rho_x is ${0}'.format(rho_x))
         if rho_x < rho_0:
             obst_cnt += 1
-            obst_vel_rep = (K_rep/(rho_x*rho_x))*((1/rho_x) - 1/rho_0)*(obst - state)/rho_x
-            #rospy.loginfo('obst_vel_rep is ${0}'.format(obst_vel_rep))
+            obst_vel_rep = (1/(rho_x*rho_x))*((1/rho_x) - 1/rho_0)*(obst - state)/rho_x
+            # rospy.loginfo('obst_vel_rep is ${0}'.format(obst_vel_rep))
             vel_rep += obst_vel_rep
     vel_rep = np.reshape(vel_rep, (2, 1))
-    vel_total = -vel_att - vel_rep
+    vel_total = -vel_att - K_rep*vel_rep
+    rospy.loginfo('vel_rep is ${0}'.format(vel_rep))
+    rospy.loginfo('vel_total is ${0}'.format(vel_total))
 
     # rospy.loginfo('obst_cnt is ${0}'.format(obst_cnt))
     # transformation to v,w
-    l = 0.1
     l_mat = np.array([[1, 0],
              [0, 1/l]])
     rot_mat = np.array([[np.cos(-theta), -np.sin(-theta)],
@@ -74,13 +86,34 @@ def odom_callback(odom_msg):
     #rospy.loginfo('l_mat is ${0}'.format(l_mat))
     #rospy.loginfo('rot_mat is ${0}'.format(rot_mat))
 
+    
+
     vw_vector = np.matmul(np.matmul(l_mat, rot_mat), vel_total)
-    if np.abs(np.amax(vw_vector)) > 1:
-        vw_vector = vw_vector / np.linalg.norm(vw_vector)
+    rospy.loginfo('vw_vector is ${0}'.format(vw_vector))
+
+    #robot maximum velocity limits
+    robot_max_linearx = 0.22
+    robot_max_rotz = 2.84
+
+    #If the calculated velocities exceed the maximum velocity limits, normalize and scale the vw_vector to the limit.
+    if np.abs(vw_vector[0]) > robot_max_linearx:
+        temp = vw_vector / np.linalg.norm(vw_vector)
+        vw_vector = (robot_max_linearx/np.abs(temp[0])) * temp
+
+    if np.abs(vw_vector[1]) > robot_max_rotz:
+        temp = vw_vector / np.linalg.norm(vw_vector)
+        vw_vector = (robot_max_rotz/np.abs(temp[1])) * temp
+
     twist = Twist()
-    twist.linear.x = 0.5*vw_vector[0]
-    twist.angular.z = 0.5*vw_vector[1]
-    # twist_pub.publish(twist)
+    twist.linear.x = vw_vector[0]
+    twist.angular.z = vw_vector[1]
+
+    # Stop if the robot is within 0.25m to the goal
+    if np.linalg.norm(goal_direction) < 0.25:
+        twist.linear.x = 0.0
+        twist.angular.z = 0.0
+
+    twist_pub.publish(twist)
 
 
 #def xy_vels_to_wv_vels(xdot, ydot):
